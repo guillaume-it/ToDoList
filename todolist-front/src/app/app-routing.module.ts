9e7051927a79e9c859ccd860e83947101afcc3a2
c82grus <ext_ruscassie.guillaume@imsa.msa.fr>
2021-07-27 19:47:11 +0200

    US 1 : List my TODOs

--- todolist-front/src/app/app-routing.module.ts
+++ todolist-front/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [
+const routes: Routes = [{
+  path: '',
+  children: [
+    { path: '', component: TodoListComponent }
+  ]
+},
+{ path: '', redirectTo: '/', pathMatch: 'full' },
+{ path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- todolist-front/src/app/todo-list/todo-list.component.ts
+++ todolist-front/src/app/todo-list/todo-list.component.ts
@@ -6,25 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['title','state', 'description'];
   dataSource = ELEMENT_DATA;
 }
-export interface PeriodicElement {
-  name: string;
+export interface TaskElement {
+  title: string;
   position: number;
-  weight: number;
-  symbol: string;
+  description: string;
+  state: boolean;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const ELEMENT_DATA: TaskElement[] = [
+  { position: 1, title: 'US 1', state:false, description: "As a user I would like to list my current todos" },
+  { position: 2, title: 'US 2', state:false, description: "Change a TODO state" },
 ];
